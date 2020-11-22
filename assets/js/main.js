fc8f28cf0d71ad5439b89c2eae1b7476f9afbfd0
felixgren <felixg.contact@gmail.com>
2020-11-23 00:55:26 +0100

    Added desktop menu & its own animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,14 @@
 
 let burger = document.querySelector('.burger');
 let overlayMobile = document.querySelector('.overlay-mobile');
+let lol = document.querySelector('.lol')
+
+const line1 = document.querySelector('.line-1');
+const line2 = document.querySelector('.line-2');
+const line3 = document.querySelector('.line-3');
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('animate-burger');
     overlayMobile.classList.toggle('overlay-visible');
+    lol.classList.toggle('lol-visible')
+    burger.classList.toggle('animate-burger');
 })
