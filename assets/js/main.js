07ac29c973ba429a3a36b9fed4a41297aa234199
felixgren <felixg.contact@gmail.com>
2020-11-23 23:29:52 +0100

    Added menu desktop anim delay & updated class names

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,19 @@
 
 const burger = document.querySelector('.burger');
 const overlayMobile = document.querySelector('.overlay-mobile');
-const lol = document.querySelector('.lol')
+const menuBar = document.querySelector('.menu-bar') 
 const line1 = document.querySelector('.line-1');
 const line2 = document.querySelector('.line-2');
 const line3 = document.querySelector('.line-3');
 
 burger.addEventListener('click', () => {
     overlayMobile.classList.toggle('overlay-visible');
-    lol.classList.toggle('lol-visible')
+    menuBar.classList.toggle('menu-bar-visible')
 
     if(burger.classList.value != 'burger animate-burger') {
         line2.style.opacity = '0';
         line1.style.transform = 'translateY(-3px)';
         line3.style.transform = 'translateY(3px)';
-
         burger.style.pointerEvents = 'none';
 
         setTimeout(() => {
@@ -26,23 +25,10 @@ burger.addEventListener('click', () => {
         }, 900);
 
     } else {
-
-            // if(burger.classList.value != 'burger animate-burger') {
-                // burger.addEventListener('mouseenter', () => {
-                //     line1.style.transform = 'translateY(-3px)';
-                //     line3.style.transform = 'translateY(3px)';
-                // })
-                // burger.addEventListener('mouseleave', () => {
-                //     line1.style.transform = 'rotate(14deg)';
-                //     line3.style.transform = 'rotate(-14deg)';
-                // })
-            // }
-
         line2.style.opacity = '0';
         line1.style.transform = 'translateY(-3px)';
         line3.style.transform = 'translateY(3px)';
         burger.classList.toggle('animate-burger');
-
         burger.style.pointerEvents = 'none';
 
         setTimeout(() => {
@@ -54,17 +40,5 @@ burger.addEventListener('click', () => {
             line3.style.transform = null;
             burger.style.pointerEvents = 'initial';
         }, 500);
-        
     }
 })
-
-// if(burger.classList.value != 'burger animate-burger') {
-//     burger.addEventListener('mouseover', () => {
-//         line1.style.transform = 'translateY(-3px)';
-//         line3.style.transform = 'translateY(3px)';
-//     })
-//     burger.addEventListener('mouseout', () => {
-//         line1.style.transform = 'translateY(0)';
-//         line3.style.transform = 'translateY(0)';
-//     })
-// }
