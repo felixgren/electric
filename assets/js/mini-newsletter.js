4e537a405ae9858b1cca72be98ba3ab94a1c0d88
felixgren <felixg.contact@gmail.com>
2020-11-27 03:46:00 +0100

    Updated mini-newsletter styling/JS & added desktop support

--- assets/js/mini-newsletter.js
+++ assets/js/mini-newsletter.js
@@ -2,17 +2,35 @@
 
 const miniNewsletterButton = document.querySelector('.mini-subscribe-btn');
 const miniNewsletterInput = document.querySelector('.mini-email-input');
-const inputValue = miniNewsletterInput.value;
 const miniNewsletterSection = document.querySelector('.mini-newsletter-wrapper');
-console.log(inputValue);
+const readMoreBtn = document.querySelector('.read-more-btn')
+const hiddenText = document.querySelector('.mini-newsletter-info')
+
+hiddenText.style = "-webkit-line-clamp: 2;";
+
+const animateReadMore = () => {
+    hiddenText.style = "-webkit-line-clamp: unset;";
+    readMoreBtn.innerHTML = "Read less";
+
+    if(hiddenText.classList.value != 'mini-newsletter-info p-visible') {
+        readMoreBtn.innerHTML = "Read more";
+
+        setTimeout(() => {
+            hiddenText.style = "-webkit-line-clamp: 2;";
+        }, 400);
+    }
+}
+
+readMoreBtn.addEventListener('click', () => {
+    miniNewsletterInput.classList.toggle('input-visible');
+    miniNewsletterSection.classList.toggle('dark-overlay');
+    hiddenText.classList.toggle('p-visible');
+    animateReadMore();
+})
 
 miniNewsletterButton.addEventListener('click', () => {
     miniNewsletterInput.classList.add('input-visible');
-    miniNewsletterSection.style.backgroundColor = 'rgba(0,0,0,0.5)';
-
+    miniNewsletterSection.classList.add('dark-overlay');
+    hiddenText.classList.add('p-visible');
+    animateReadMore();
 })
-
-function controlInput() {
-    console.log('hej');
-    console.log(inputValue)
-}
